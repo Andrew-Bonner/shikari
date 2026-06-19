f391c755812e189c39a0d2d82f2173137c0c2162
Andrew-Bonner <andrew.bonner@bsu.edu>
2026-06-19 16:39:55 -0700

    incorperated shodan into a seach

--- core.py
+++ core.py
@@ -3,11 +3,13 @@ from google import genai
 
 HUNTER_IO_API_KEY = '52f0cae3795354e834485a5ebb67413fb6700407'
 VT_API_KEY = 'bdbec77bbec001e0eeaeb085973a930f1cc0a8a2c0c31b9cee094310b196e287'
+SHODAN_API_KEY = 'zuskxF9kEtDzDIEQ96OJTRcmHrKyZc48'
+SHODAN_URL = 'https://api.shodan.io/dns/domain/'
 vt_url = 'https://www.virustotal.com/api/v3/domains/'
-hunterio_url='https://api.hunter.io/v2/email-verifier?email='
+hunterio_url='https://api.hunter.io/v2/email-verifier?'
 
 def ask_gemini(message_content):
-    client = genai.Client(api_key='AIzaSyCsXDV5xSXISLczruH4os3h00_vBILY3Aw')
+    client = genai.Client(api_key='AIzaSyBbLTqRnyEH0DKAWok_lVo6iLFu6RW2Yt4')
 
     response = client.models.generate_content(
         model='gemini-2.5-flash',
@@ -25,14 +27,27 @@ def get_domain_data(domain,api_key):
     return repsonse.text
 
 def get_email_data(email):
-    repsonse = requests.get(f'{hunterio_url}?email={email}&api_key={HUNTER_IO_API_KEY}')
-    return repsonse.json
+    params = {
+        'api_key':HUNTER_IO_API_KEY,
+        'email':email
+    }
+    repsonse = requests.get(hunterio_url,params=params)
+    return repsonse.json()
 
+def get_shodan_domain(domain):
+    SHODAN_URL = f'https://api.shodan.io/dns/domain/{domain}'
+    params = {
+        'key': SHODAN_API_KEY,
+    }
+    response = requests.get(SHODAN_URL,params=params)
+    return response.json()
+    
 
-domian = input('type your domain: ')
-vt_content = f'summarize the json data returned from this VirusTotal search about the domain in an organized format for a terminal: {get_domain_data(domian,VT_API_KEY)}'
-model_response = ask_gemini(vt_content)
+domain = input('type your domain: ')
+#vt_content = f'summarize the json data returned from this VirusTotal search about the domain in an organized format for a terminal: {get_domain_data(domian,VT_API_KEY)}'
+shodan_content = f'summarize the json data returned from this shodan search on a domain and organize it in a format that can be viewed in the terminal: {get_shodan_domain(domain)}'
+model_response = ask_gemini(shodan_content)
 print(model_response)
-email = input('type in an email: ')
-hunter_content = f'summarize the json data returned from this Hunter.io search about the email in an organized format for a terminal: {get_email_data(email)}'
-print(ask_gemini(hunter_content))
+#email = input('type in an email: ')
+#hunter_content = f'convert the json data into a summary about the email in an organized format for a terminal: {get_email_data(email)}'
+#print(ask_gemini(hunter_content))
